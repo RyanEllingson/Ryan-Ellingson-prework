7ca111b599d636ee54bb773e671814be81a90e2a
Geddy Delonge <57271262+RyanEllingson@users.noreply.github.com>
2020-05-25 21:43:08 -0500

    opening message goes away when you type on the keyboard

---
+++ Module-2_Assessment/assets/javascript/game.js
@@ -0,0 +1,15 @@
+const initializeGame = function() {
+    const openingMessageEl = document.getElementById("opening-message");
+    openingMessageEl.className = "d-none";
+}
+
+// Display intro screen, listen for keyboard entry - on keyboard entry, remove intro screen and start game
+document.addEventListener("keydown", () => {
+    initializeGame();
+});
+// Initialize list of questions
+// Randomly choose a question
+// Display question, number of remaining guesses, and incorrect guesses
+// Initialize letter objects that make up the word, display word on the screen as all blanks
+// Each time a letter is guessed, check whether letter exists in word - if yes, reveal letter in the word; if no, add word to incorrect guesses and deduct from remaining guesses
+// When word is complete, display Game Win message, choose new word, and start game again
